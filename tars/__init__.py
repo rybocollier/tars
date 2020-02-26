72afbaf57dc26a1050b7ab1f121bf4c4c64cbfa4
Ryan Collier <rybocollier@gmail.com>
2020-02-25 20:47:57 -0800

    Adding greeting support

--- tars/__init__.py
+++ tars/__init__.py
@@ -5,6 +5,7 @@ import sys
 import time
 from flask import Flask, request, Response
 from slackclient import SlackClient
+from slackeventsapi import SlackEventAdapter
 
 # instantiate flask app
 app = Flask(__name__)
@@ -19,27 +20,37 @@ slack_client = SlackClient(SLACK_TOKEN)
 def print_debug(input):
     print(input, file=sys.stderr)
 
-@app.route('/slack', methods=['POST'])
-def verify_request():
-    slack_signing_secret = SLACK_SIGNING_SECRET
-    request_data = request.get_data()
-    challenge_timestamp = request.headers['X-Slack-Request-Timestamp']
-    slack_signature = request.headers['X-Slack-Signature']
-    
-    if abs(time.time() - int(challenge_timestamp)) > 60 * 5:
-        return
+slack_events_adapter = SlackEventAdapter(SLACK_SIGNING_SECRET, "/slack/events", app)
 
-    sig_basestring = str.encode('v0:' + str(challenge_timestamp) + ':') + request_data
+# Create an event listener for "reaction_added" events and print the emoji name
+@slack_events_adapter.on("reaction_added")
+def reaction_added(event_data):
+  emoji = event_data["event"]["reaction"]
+  print_debug(emoji)
 
-    tars_signature = 'v0=' + hmac.new(
-        str.encode(slack_signing_secret),
-        sig_basestring, hashlib.sha256
-    ).hexdigest()
+@slack_events_adapter.on("member_joined_channel")
+def member_joined(event_data):
+    event = event_data["event"]
 
-    if hmac.compare_digest(tars_signature, slack_signature):
-        return request_data
-    
-    return False
+    # match user ID to user name
+    user_data = slack_client.api_call(
+        "users.info",
+        token=SLACK_TOKEN,
+        user=event["user"],
+    )
+
+    # match channel ID to channel name
+    channel_data = slack_client.api_call(
+        "channels.info",
+        token=SLACK_TOKEN,
+        channel=event["channel"],
+    )
+
+    message = "Welcome " + user_data["user"]["name"] + " to " + channel_data["channel"]["name"] + "!"
+
+    slack_client.api_call("chat.postMessage", channel=event["channel"], text=message)
+
+    return event
 
 @app.route('/', methods=['GET'])
 def test():
